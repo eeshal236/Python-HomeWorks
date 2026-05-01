6640f18a4b8ec225bcad8c0868d112eab483aada
eeshal236 <eeshalkhan646@gmail.com>
2026-05-01 10:30:42 +0500

    initial commit

---
+++ string.py
@@ -0,0 +1,25 @@
+name="Eeshal"
+age=12
+school="Govt.Girls High School"
+cl=8
+weight=37.9
+height=5.5
+course="Python"
+color="Purple"
+father_name="Qadeer"
+flower="Jasmine"
+hobby="Story Reading"
+subject="Mathematics"
+
+print("My name is ",name)
+print("My father name is ",father_name)
+print("I am ",age,"years old")
+print("I read in the school called ",school)
+print("I am in grade ",cl)
+print("MY favourite subject is ",subject)
+print("My height is ",height)
+print("My weight is ",weight)
+print("My favourite color is ", color)
+print("My hobby is ",hobby)
+print("I like ",flower,"flower")
+print("My current course is ",course)
---
+++ var.py
@@ -0,0 +1,6 @@
+a = 200
+b=100
+print("The addition is:",a+b)
+print("The subtraction is:",a-b)
+print("The product is:",a*b)
+print("The division is:",a/b)
