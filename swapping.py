62fb09187d78dfb672dcaf667b01882b970fa8f8
eeshal236 <eeshalkhan646@gmail.com>
2026-05-02 09:34:23 +0500

    initial commit

---
+++ swapping.py
@@ -0,0 +1,31 @@
+#Activity 1
+numbers = [5,9,1]
+numbers = sorted(numbers)
+
+print(numbers)
+#Activity 2
+x = 5
+y = 9
+z = 1
+print(x)
+print(y)
+print(z)
+print("")
+print(x,y,z)
+print("Ascending order ⬇")
+x,y,z = z,x,y
+
+print("✅",x,y,z)
+#Activity 3
+a = "a"
+b = "h"
+c = "s"
+d = "E"
+e = "l"
+f = "e"
+
+print("Swap this ➡",a,b,c,d,e,f)
+
+a,b,c,d,e,f = d,f,c,b,a,e
+
+print("The correct name is ✅",a,b,c,d,e,f)
